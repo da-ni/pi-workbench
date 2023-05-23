527abc6607b4afaaaa7939cbc0efcfb08600285b
Ilanea <Ilanea@users.noreply.github.com>
2023-05-23 19:11:37 +0200

    switch to fastapi
    - run it
    - go to http://127.0.0.1:8000/api/docs

--- linus.py
+++ linus.py
@@ -1,7 +1,13 @@
 import argparse
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.openapi.docs import get_redoc_html
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import HTMLResponse
+
+from pydantic import BaseModel
 from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
@@ -9,61 +15,78 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 import textwrap
 
-# Define the command line arguments
-parser = argparse.ArgumentParser(description='Run the Linux Language Model with an example prompt.')
-parser.add_argument('--output-dir','-o', default='db', type=str, help='The directory of the vector database.')
+app = FastAPI()
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+class PromptResponse(BaseModel):
+    result: str
+    sources: list
 
 def wrap_text_preserve_newlines(text, width=110):
     lines = text.split('\n')
     wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
     wrapped_text = '\n'.join(wrapped_lines)
-    
     return wrapped_text
 
-
 def process_llm_response(llm_response):
-    print(wrap_text_preserve_newlines(llm_response['result']))
-    print('\n\nSources:')
-    for source in llm_response["source_documents"]:
-        print(source.metadata['source'])
+    wrapped_result = wrap_text_preserve_newlines(llm_response['result'])
+    sources = [source.metadata['source'] for source in llm_response["source_documents"]]
+    return PromptResponse(result=wrapped_result, sources=sources)
 
+@app.post("/api/prompt", response_model=PromptResponse)
+def run_prompt(request: PromptRequest):
+    prompt = request.prompt
 
-def main():
+    # Run the chain
+    llm_response = qa_chain(prompt)
+    processed_response = process_llm_response(llm_response)
+
+    return processed_response
+
+if __name__ == "__main__":
+    # Define the command line arguments
+    parser = argparse.ArgumentParser(description='Run the Linux Language Model with an example prompt.')
+    parser.add_argument('--output-dir','-o', default='db', type=str, help='The directory of the vector database.')
+
     # Parse the command line arguments
     args = parser.parse_args()
 
     # Load the API keys
     load_dotenv()
 
-    # load the vector store
+    # Load the vector store
     persist_directory = args.output_dir if len(args.output_dir) else os.environ['PERSIST_DIRECTORY']
-    print(persist_directory)
-    embeddings = OpenAIEmbeddings() # type: ignore
+    embeddings = OpenAIEmbeddings()
     vectordb = Chroma(
-        persist_directory=persist_directory, 
+        persist_directory=persist_directory,
         embedding_function=embeddings
     )
 
-    # create retriever
+    # Create the retriever
     retriever = vectordb.as_retriever()
 
-    # create chain 
+    # Create the chain
     qa_chain = RetrievalQA.from_chain_type(
-        llm=OpenAI(),  # type: ignore
-        chain_type="stuff", 
-        retriever=retriever, 
+        llm=OpenAI(),
+        chain_type="stuff",
+        retriever=retriever,
         return_source_documents=True
     )
 
-    while True:
-        prompt = input("\nEnter a prompt: ")
-        if prompt == "exit":
-            break
+    @app.get("/api/docs", response_class=HTMLResponse)
+    async def custom_docs():
+        return get_redoc_html(openapi_url="/api/openapi.json", title="API Documentation")
 
-        # run the chain
-        llm_response = qa_chain(prompt)
-        process_llm_response(llm_response)
+    @app.get("/api/openapi.json")
+    async def get_open_api_endpoint():
+        return get_openapi(
+            title="API Documentation",
+            version="0.0.1",
+            description="This is an API for running prompts and retrieving results.",
+            routes=app.routes,
+        )
 
-
-if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
