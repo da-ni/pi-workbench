8b01d2e8358518ecedfce52ca1c9418a1d56bb32
PhilippAuss <philipp.ausserlechner@gmail.com>
2023-06-01 10:34:20 +0200

    chatbot_artist_agent.py is fixed, now pretends to Pieter Bruegel and only answers questions about underlying corpus

--- chatbot_artist_agent.py
+++ chatbot_artist_agent.py
@@ -13,6 +13,11 @@ from langchain.chains import RetrievalQAWithSourcesChain, ChatVectorDBChain, Con
 from langchain.prompts import PromptTemplate
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.memory import ConversationBufferMemory
+from langchain.agents import Tool
+from langchain.agents import AgentType
+
+from langchain.utilities import SerpAPIWrapper
+from langchain.agents import initialize_agent
 
 
 # Define the command line arguments
@@ -53,163 +58,63 @@ def main():
 
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-    llm = OpenAI(temperature=0.,
-                openai_api_key=os.environ['OPENAI_API_KEY'])
-
-    qa = ConversationalRetrievalChain.from_llm(llm,
-                                               vectordb.as_retriever(),
-                                               memory = memory)
-                                            #    return_source_documents=True)
     
-    result = qa({'question':'Wer ist Bruegel?'})
-
-    print("hello")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    docs = vectordb.similarity_search_with_score("Wer ist Bruegel?")
-
-    # memory = ConversationBufferMemory()
-
-    # llm = ChatOpenAI(
-    #     openai_api_key=os.environ['OPENAI_API_KEY'],
-    #     model_name='gpt-3.5-turbo',
-    #     temperature=0.
-    #     )
-
-
-    
-
-
-    template = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). 
-If you don't know the answer, just say that you don't know. Don't try to make up an answer.
-ALWAYS return a "SOURCES" part in your answer.
-Respond in the style of the time in which the artist lived, which the question and the sources are about.
-Pretend that you are the artist from ("SOURCES") and we are having a Dialog.
-Respond in the language the ("QUESTION") was asked.
-
-QUESTION: {question}
-=========
-{summaries}
-=========
-FINAL ANSWER IN GERMAN:"""
-
-    PROMPT = PromptTemplate(template=template, input_variables=["summaries", "question"])
-
-    chain = load_qa_with_sources_chain(llm=llm, chain_type='stuff',prompt=PROMPT)
-
-    results = chain({'input_documents':docs, 'question':"Wer ist Breugel?"})
-
-    print("Hello")
-
-
-
-
-    # # create retriever
-    # retriever = vectordb.as_retriever()
-
-
-    # docs = retriever.get_relevant_documents(query="Wer ist Bruegel?")[:3]
-
-
-
-
-    from langchain.agents import Tool
-    from langchain.agents import AgentType
-    
-    from langchain.utilities import SerpAPIWrapper
-    from langchain.agents import initialize_agent
-
-    tools = [Tool(
-                  name="Information about you the artist",
-                  func = lambda q: str(retriever.get_relevant_documents(q)),
-                  description="use always the information from this artist data, if there is no helpful information say: Ich weiss das nicht!"
-                ),]
-    
-    memory = ConversationBufferMemory()
-
-
-    # completion llm
+    # chat completion llm
     llm = ChatOpenAI(
-            openai_api_key=os.environ['OPENAI_API_KEY'],
-            model_name='gpt-3.5-turbo',
-            temperature=0.
+        openai_api_key=os.environ['OPENAI_API_KEY'],
+        model_name='gpt-3.5-turbo',
+        temperature=0.0
+    )
+
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectordb.as_retriever()
+    )
+    
+    tools = [
+        Tool(
+            name='Artist Database',
+            func=qa.run,
+            description=(
+                'use this tool when answering questions about the artist queries to get information about that topic'
+                'If the question is not about the artist just answer: Ich weiss das nicht!'
+                'Always answer in German!'
             )
+        )
+    ]
     
-    agent_chain = initialize_agent(tools, 
-                                   llm, 
-                                   agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
-                                   verbose=True,
-                                   memory=memory)
-
-
-
-    # qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
-    #                     llm=llm,
-    #                     chain_type="stuff",
-    #                     retriever=retriever
-    #                     )
+    agent = initialize_agent(
+        agent='chat-conversational-react-description',
+        tools = tools,
+        llm = llm,
+        verbose=True,
+        max_iterations=3,
+        early_stopping_method='generate',
+        memory=memory
+    )
     
-    template = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). 
-If you don't know the answer, just say that you don't know. Don't try to make up an answer.
-ALWAYS return a "SOURCES" part in your answer.
-Respond in the style of the time in which the artist lived, which the question and the sources are about.
-Pretend that you are the artist from ("SOURCES") and we are having a Dialog.
-Respond in the language the ("QUESTION") was asked.
-
-QUESTION: {question}
-=========
-{summaries}
-=========
-FINAL ANSWER IN GERMAN:"""
-
-    # COMBINE_PROMPT = PromptTemplate(template=template, input_variables=["question","summaries"])
+    sys_msg="""You are Peter Bruegel and you will use a language style which is apropriate to the time of the Artist, when answering questions.
+    You are only anwsering questions with the help of your tool, which leads you extract information from a database.
+    Always use I instead of Bruegel! 
+    If no information is found in the database you simply answer: Ich weiss das nicht!
+    """
     
-    # qa = ConversationalRetrievalChain.from_llm(llm = llm, 
-    #                                            retriever= retriever, 
-    #                                            condense_question_prompt=COMBINE_PROMPT,
-    #                                            chain_type='map_reduce')
+    new_prompt = agent.agent.create_prompt(system_message=sys_msg, tools=tools)
     
-
-    # llm_response = qa_with_sources("Who is Bruegel?")
-    # process_llm_response(llm_response)
-
-
-    from langchain.schema import BaseMessage
-
-
+    agent.agent.llm_chain.prompt=new_prompt
     
-
+    
+    
     while True:
         query = input("\nEnter a question: ")
-        chat_history = []
         if query == "exit":
             break
 
-        chat_history.append(query)
-
-
-        # run the chain
-        # llm_response = qa_with_sources(prompt)
-        # llm_response = qa({"chat_history":chat_history, 
-        #                    "question":query})
-        llm_response=agent_chain.run(query)
+        llm_response=agent(query)
         
-        print(llm_response['answer'])
-        # chat_history.append(llm_response['answer'])
+        print(llm_response['output'])
+
         
 if __name__ == "__main__":
     main()
