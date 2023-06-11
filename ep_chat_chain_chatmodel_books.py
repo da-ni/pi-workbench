50214f9c91f35ed2ae5f1d539ea0bd58d27ef57e
Eric <eric.pr@live.de>
2023-06-11 17:10:04 +0200

    adds chat memory to ep_chat_chain_chatmodel_books

--- ep_chat_chain_chatmodel_books.py
+++ ep_chat_chain_chatmodel_books.py
@@ -5,6 +5,7 @@ from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.callbacks import get_openai_callback
+from langchain.memory import ChatMessageHistory
 #from langchain.schema.Document import Document
 
 
@@ -31,6 +32,11 @@ def main():
 
     retriever = vectordb.as_retriever()
 
+    chat_history = ""
+    #chat_history = ChatMessageHistory()
+    #chat_history.add_message("Hi ich bin Lukas")
+    #chat_history.add_ai_message("Hey Heyyy :) bin Pieter Bruegel")
+
     #initialize the llm
     #llm = OpenAI(temperature=0)
 
@@ -45,28 +51,34 @@ def main():
 
     #template for the similarty search
     prepare_prompt = PromptTemplate(
-        input_variables=["query"],
+        input_variables=["query", "chat_history"],
         template ="""  You are Pieter Bruegel. You must only reply the final translated and rephrased question \
                        Use this question of a user and translate it to ENGLISH \
                        then restructure it to the third person view \
                        then rephrase the question in a way that is relevant for a similarity search in the documents \
-                       This is the question from the user: {query} + \n\n \
-                       Only reply Rephrased for similarity search: <response>"""
+                       This is the question from the user: \n{query} + \n\n \
+                       For additional context this is the conversation between you and the user so far: \n{chat_history}\n\n"""
     )
 
     #chain = LLMChain(llm=llm, prompt=prepare_prompt, verbose=True)
-    optimize_query_chain = LLMChain(llm=llmSimilarty, prompt=prepare_prompt, verbose=True)
+    optimize_query_chain = LLMChain(llm=llmSimilarty, prompt=prepare_prompt, verbose=False)
 
     finalPrompt = PromptTemplate(
-        input_variables=["documents", "query"],
-        template ="""  You are the GERMAN speaking Pieter Bruegel and answer in the first person with information that is provided. \
+        input_variables=["documents", "chat_history", "query"],
+        template ="""  You are the GERMAN speaking Pieter Bruegel are haveing a conversation with a user. \
+                                Answer in the first person with information that is provided through the DOCUMENTS. \
                                 Use the linguistic style of people living in Germany in th 16th century. \
-                                \n These are your documents:\n{documents} \n
+                                \n These are your DOCUMENTS:\n{documents}\n\n
 
-                                \n\n\n  This is your information to respond to the message: {query}\n"""
+                                \n This is the conversation between you and the user so far: \n{chat_history}\n\n
+
+                                \n This is your next message to respond to: \n{query}\n"""
     )
 
-    response_chain = LLMChain(llm=llmPieter, prompt=finalPrompt, verbose=True)
+
+
+      #                          
+    response_chain = LLMChain(llm=llmPieter, prompt=finalPrompt, verbose=False)
      
                     
 
@@ -84,21 +96,32 @@ def main():
 
         #returns string
         with get_openai_callback() as cb:
-            searchPrompt = optimize_query_chain.run({"query": query})
+            searchPrompt = optimize_query_chain.run({"query": query, "chat_history": chat_history})
             print(f"\nCallback: {cb}\n\n")
-        print(searchPrompt)
+        print("The Promt for similarity search:  " + searchPrompt + "\n\n")
 
         relevantDocuments = vectordb.similarity_search(searchPrompt, k=3)
 
-        print(relevantDocuments)
+        #print(relevantDocuments)
 
 
         with get_openai_callback() as cb:
-            pieteResponse= response_chain({"documents": relevantDocuments, "query": query})
+            pieteResponse= response_chain({"documents": relevantDocuments, "chat_history": chat_history, "query": query})
+            #pieteResponse= response_chain({"documents": relevantDocuments, "query": query})
             print(f"\nCallback: {cb}\n")
+
+        print("The final prompt:  " + searchPrompt + "\n\n")
+        
+        
         print(pieteResponse['text'])
 
         
+        chat_history += f"User message: {query} \n"
+        chat_history += f"AI_response: {pieteResponse['text']} \n"
+        
+        #chat_history.add_user_message(query)
+        #chat_history.add_ai_message(pieteResponse['text'])
+                
 
         #for r in relevantDocuments:
         #    if isinstance(r, Document):
