3ec04112b1053321fe0123fea74732b84b477376
Ilanea <Ilanea@users.noreply.github.com>
2023-06-30 19:27:27 +0200

    add reset chatbot

--- main_CLI_elevenlabs.py
+++ main_CLI_elevenlabs.py
@@ -2,22 +2,40 @@ import os
 import uuid
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from LLM_Pieter import LLM_Pieter
 from elevenlabs import generate, save
 
 app = FastAPI()
-pieter_chat_bot = LLM_Pieter()
+pieter_chat_bot = None
+
+origins = [
+    "127.0.0.1",
+    "0.0.0.0",
+    "localhost:8000",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     message: str
 
+def initialize_pieter_chat_bot():
+    global pieter_chat_bot
+    pieter_chat_bot = LLM_Pieter()
+
 @app.post("/chat")
 def chat(message: Message):
     user_input = message.message
-
-    if user_input == "exit":
-        return {"response": "Goodbye!"}
 
     response = pieter_chat_bot.run_query(user_input)
 
@@ -36,5 +54,11 @@ def chat(message: Message):
 
     return {"response": response, "audio_path": audio_path}
 
+@app.get("/chat/reset")
+def reset_chat_bot():
+    initialize_pieter_chat_bot()
+    return {"message": "Chat bot has been reset."}
+
 if __name__ == "__main__":
+    initialize_pieter_chat_bot()
     uvicorn.run("main_CLI_elevenlabs:app", host="127.0.0.1", port=8000)
